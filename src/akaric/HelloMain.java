85809dfaaa81e57fc8261916c5e1fbfd617028c0
Adin Karic <akaric@student.tgm.ac.at>
2014-03-02 16:14:50 +0100

    kommentar

---
+++ src/akaric/HelloMain.java
@@ -0,0 +1,18 @@
+
+package akaric;
+/**
+ * Main Klasse
+ * @author Adin Karic
+ * @version 2014-03-02
+ */
+public class HelloMain {
+	/**
+	 * main methode
+	 * @param args
+	 */
+	public static void main(String[] args){
+		HelloWorld.halloAdin();
+		HelloWorld.halloTGM();
+		HelloWorld.hallo3BHIT();
+	}
+}
---
+++ src/akaric/HelloWorld.java
@@ -0,0 +1,26 @@
+package akaric;
+/**
+ * Klasse die Hallo sagt
+ * @author Adin Karic
+ * @version 2014-03-02
+ */
+public class HelloWorld {
+	/**
+	 * Methode die "Hallo Adin!" ausgibt
+	 */
+	public static void halloAdin(){
+		System.out.println("Hallo Adin!");
+	}
+	/**
+	 * Methode die "Hallo TGM!" ausgibt
+	 */
+	public static void halloTGM(){
+		System.out.println("Hallo TGM!");
+	}
+	/**
+	 * Methode die "Hallo 3BHIT!" ausgibt
+	 */
+	public static void hallo3BHIT(){
+		System.out.println("Hallo 3BHIT!");
+	}
+}
